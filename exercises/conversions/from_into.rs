5224dc68770787ee9d40cca2fcd1ea9583c5537a
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-29 12:11:20 +0200

    rustlings, from/into, my solution is ugly but working XD TBD: REFACTOR

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        } else {
+            if let Some((name, age)) = s.split_once(",")
+            {
+                let name = name.to_string();
+                if name == "" {
+                    return Person::default();
+                } else {
+                    let age = age.parse::<usize>();
+                    if let Ok(age) = age {
+                        return Person {
+                            name,
+                            age,
+                        }
+                    } else {
+                        return Person::default();
+                    }
+                }
+            } else {
+                return Person::default();
+            }
+        }
     }
 }
 
